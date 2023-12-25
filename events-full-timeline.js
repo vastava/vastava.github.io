6a51a7b8ae852aa87341cda96f9f77924934cd4e
Anjali <ashrivastava@berkeley.edu>
2023-12-25 14:05:47 -0800

    axis labeling

--- events-full-timeline.js
+++ events-full-timeline.js
@@ -1,5 +1,6 @@
 var fullMargin = {top: 30, right: 30, bottom: 30, left: 30},
-    fullHeight = 764 - fullMargin.left - fullMargin.right,
+    fullHeight = 764 
+	// - fullMargin.left - fullMargin.right,
     fullWidth = (1000 - fullMargin.top - fullMargin.bottom);
 
 // append the canoneventline object to the body of the page
@@ -21,11 +22,7 @@ var container = d3.select("#events_full_timeline")
     .style("margin-right", "auto")
     .style("top", function(d) { return fullMargin.top + "px"; })
     .style("left", function(d) { return fullMargin.left + "px"; })
-        // .append("div")
-        // .attr("width", fullWidth + fullMargin.left + fullMargin.right)
-        // .attr("height", fullHeight + fullMargin.top + fullMargin.bottom)    
-        // .attr("id", "relative-container")   
-        // .style("position", "relative");
+
 //Build checkboxes
 var filter_list = ["Original Trilogy", "Prequel Trilogy", "Sequel Trilogy", "Anthology Films", "The Clone Wars", "The Mandalorian", "Rebels", "Resistance", "Extended Universe"]
 
@@ -34,22 +31,22 @@ var media_color = d3.scaleOrdinal()
 		 .domain(filter_list)
 		 .range(swcolors_media)
 
-d3.select("#filter")
-    .selectAll("input")
-    .data(filter_list)
-    .enter()
-    .append("label")
-    .attr("class", "switch")
-    .append("input")
-    .attr("type", "checkbox")
-    .attr("class", "filter-check")
-    .property("checked", true)
-    .attr("value", function (d) {
-        return d
-    })
-    .attr("id", function (d) {
-        return d
-    });
+// d3.select("#filter")
+//     .selectAll("input")
+//     .data(filter_list)
+//     .enter()
+//     .append("label")
+//     .attr("class", "switch")
+//     .append("input")
+//     .attr("type", "checkbox")
+//     .attr("class", "filter-check")
+//     .property("checked", true)
+//     .attr("value", function (d) {
+//         return d
+//     })
+//     .attr("id", function (d) {
+//         return d
+//     });
 
 // var spans = d3.selectAll("label")
 //     .data(filter_list)
@@ -103,8 +100,8 @@ function loadTimeline(choices, filter) {
 		d3.select("#vline").remove();
 		d3.selectAll(".svg-label").remove()
 
-		data = data.sort(function(a,b) {return a.start - b.start});
-		var years = d3.set(data.map(function(d) {return d.start})).values();
+		data = data.sort(function(a,b) {return a.yr_cleaned - b.yr_cleaned});
+		var years = d3.set(data.map(function(d) {return d.yr_cleaned})).values();
 		// var sz = (fullHeight - fullMargin.bottom - fullMargin.top)/years.length;        
         var sz = (fullWidth - fullMargin.left - fullMargin.right)/years.length;
         console.log(sz)
@@ -138,7 +135,11 @@ function loadTimeline(choices, filter) {
 		    data = csv; // so that no boxes checked shows all data
 		}	
 
-		var years = d3.set(data.map(function(d) {return d.start})).values();
+		var years = d3.set(data.map(function(d) {return d.yr_cleaned})).values();
+
+		years.sort(function(a, b) {
+			return b - a;
+		});		
 
 		var y = d3.scaleBand()
 			.domain(years)
@@ -148,17 +149,17 @@ function loadTimeline(choices, filter) {
 			.domain([-d3.max(data, function(d) { return +d["y-key"]; }), d3.max(data, function(d) { return +d["y-key"]; })])
 			.range([0, fullWidth - fullMargin.left - fullMargin.right])
 
-		var startDate = d3.min(data, function(d) { return +d.start; });
-		var endDate = d3.max(data, function(d) { return +d.start; });
-		var num_days = (endDate)- (startDate)
-		var w_axis = x(endDate)-x(startDate)
+		var yr_cleanedDate = d3.min(data, function(d) { return +d.yr_cleaned; });
+		var endDate = d3.max(data, function(d) { return +d.yr_cleaned; });
+		var num_days = (endDate)- (yr_cleanedDate)
+		var w_axis = x(endDate)-x(yr_cleanedDate)
 
 
 	    // var movieData = []
 
-	    // movieData[0] = {"start": -32, "end": -19, "section": 2, "text": "Prequel Trilogy", "text_x": -25}
-	    // movieData[1] = {"start": 0, "end": 4, "section": 3, "text": "Original Trilogy", "text_x": 0}
-	    // movieData[2] = {"start": 34, "end": 35, "section": 3, "text": "Sequel Trilogy", "text_x": 34}	
+	    // movieData[0] = {"yr_cleaned": -32, "end": -19, "section": 2, "text": "Prequel Trilogy", "text_x": -25}
+	    // movieData[1] = {"yr_cleaned": 0, "end": 4, "section": 3, "text": "Original Trilogy", "text_x": 0}
+	    // movieData[2] = {"yr_cleaned": 34, "end": 35, "section": 3, "text": "Sequel Trilogy", "text_x": 34}	
 	    
 	    // svg.selectAll(".myRects")
 	    // 	.data(movieData)
@@ -166,9 +167,9 @@ function loadTimeline(choices, filter) {
 	    // 	.append("rect")
 	    // 	.attr("class", "movie-rect")
 	    // 	.attr("x", 0)
-	    // 	.attr("y", function(d) {return y(d.start)})		
+	    // 	.attr("y", function(d) {return y(d.yr_cleaned)})		
 	    // 	.attr("width", fullWidth)
-	    // 	.attr("height", function(d) {return y(d.end) - y(d.start)})
+	    // 	.attr("height", function(d) {return y(d.end) - y(d.yr_cleaned)})
 	    // 	.style("fill", function(d) {
 	    // 		if (d.text != "Sequel Trilogy") {
 	    // 			return media_color(d.text);
@@ -183,15 +184,15 @@ function loadTimeline(choices, filter) {
 	    // 	.append("text")
 	    // 	.attr("class", "movie-rect")
 	    // 	.attr("x", 0+5)
-	    // 	.attr("y", function(d) {return y(d.start)-5})		
+	    // 	.attr("y", function(d) {return y(d.yr_cleaned)-5})		
 	    // 	.text(function(d) {return d.text});
 
-		var yrs = [0, 1, 4]
+		var yrs = []
 		var events = ["okokok", "lalala", "hahahah"]	
 
 		yrs = yrs.map( function(x, i) { return {"year": x, "event": events[i]}});
 
-		var yrs_canon = [3];
+		var yrs_canon = [];
 		var events_canon = ["mymymy"];
 
 		yrs_canon = yrs_canon.map( function(x, i) { return {"year": x, "event": events_canon[i]}});
@@ -210,9 +211,7 @@ function loadTimeline(choices, filter) {
 		 	.attr("x1", x(0) + sz*2)
 		 	.attr("x2", x(0) + 300)  
 		 	.attr("z-index", 0)
-		 	// .attr("y1", y(-13000000000)+sz*spacing/4)
 		 	.attr("y1", function(d) {return y(d.year) + sz*spacing/4})
-		 	// .attr("y2", y(-13000000000)+sz*spacing/4)
 		 	.attr("y2", function(d) {return y(d.year) + sz*spacing/4})
 		 	.attr("stroke", "black")
 		 	.attr("stroke-width", 1)
@@ -224,7 +223,6 @@ function loadTimeline(choices, filter) {
 			.attr("class", "svg-label")
 		 	.attr("x", x(0) + sz*2)
 		 	.attr("x", x(0) + 300)  
-		 	// .attr("y", y(-13000000000)+sz*spacing/4)
 		 	.attr("y", function(d) {return y(d.year) + sz*spacing/4})
 		 	.text(function(d) {return d.event})	
 		 	.attr("text-anchor", "end") 
@@ -240,7 +238,6 @@ function loadTimeline(choices, filter) {
 		 	.attr("x", x(0) + 300)  
 		 	// .attr("y", y(-13000000000)+sz*spacing/4 - 5)
 		 	.attr("y", function(d) {return y(d.year) + sz*spacing/4 - 5})
-		 	.text("13,000,000 BBY")	
 		 	.text(function(d) {return formatDate(d.year)})
 		 	.attr("text-anchor", "end") 
 		 	// .attr("alignment-baseline", "hanging")	
@@ -269,7 +266,7 @@ function loadTimeline(choices, filter) {
 		 	// .attr("y", y(-13000000000)+sz*spacing/4)
 		 	.attr("y", function(d) {return y(d.year) + sz*spacing/4})
 		 	.text(function(d) {return d.event})	
-		 	.attr("text-anchor", "start") 
+		 	.attr("text-anchor", "yr_cleaned") 
 		 	.attr("alignment-baseline", "hanging")	
 		 	.style("font-weight", "lighter")	
 
@@ -283,14 +280,72 @@ function loadTimeline(choices, filter) {
 		 	// .attr("y", y(-13000000000)+sz*spacing/4 - 5)
 		 	.attr("y", function(d) {return y(d.year) + sz*spacing/4 - 5})
 		 	.text(function(d) {return formatDate(d.year)})
-		 	.attr("text-anchor", "start") 
+		 	.attr("text-anchor", "yr_cleaned") 
 		 	// .attr("alignment-baseline", "hanging")	
 		 	.style("font-weight", "lighter")	
+
+		svg.append("line")
+			 .attr("id", "vline")
+			  .attr("x1", x(0) + sz/(spacing*2))
+			  .attr("x2", x(0) + sz/(spacing*2))  
+			  .attr("y1", 0)
+			  .attr("y2", fullHeight - fullMargin.top - fullMargin.bottom) 
+			  .attr("stroke", "black")
+			  .attr("stroke-width", 1)	
+			  .attr("tranform", "translate(" + sz + "," + sz + ")")
+ 
+		 svg.append("line")
+			  .attr("x1", x(0) - 30 + sz/(spacing*2))
+			  .attr("x2", x(0) + 30 + sz/(spacing*2))  
+			  .attr("y1", 0 - 5)
+			  .attr("y2", 0 - 5) 
+			  .attr("stroke", "black")
+			  .attr("stroke-width", 1)	
+			  // .attr("tranform", "translate(" + sz/2 + "," + -sz + ")")		 	
+ 
+ 
+		 svg.append("line")
+			  .attr("x1", x(0) - 30 + sz/(spacing*2))
+			  .attr("x2", x(0) + 30 + sz/(spacing*2))  
+			  .attr("y1", fullHeight - fullMargin.top - fullMargin.bottom + 5)
+			  .attr("y2", fullHeight - fullMargin.top - fullMargin.bottom + 5)
+			  .attr("stroke", "black")
+			  .attr("stroke-width", 1)			
+
+		svg.selectAll(".label")
+			.data(years)
+			.enter()
+			.append("rect")
+			.attr("class", "svg-label")
+			.attr("x", x(0) + 30)	
+			.attr("y", function(d) {return y(d) + sz/(spacing*2) - 15})
+			.attr("width", "30px")
+			.attr("height", "17px")
+			// .style("font-weight", "lighter")
+			.style("fill", "white")
+			// .style("stroke", "white") 
+			// .style("stroke-width", "0.5px")		
+			.style("z-index", 20)				  
+
+		svg.selectAll(".label")
+			.data(years)
+			.enter()
+			.append("text")
+			.attr("class", "svg-label")
+			.attr("x", x(0) + 30)	
+			.attr("y", function(d) {return y(d) + sz/(spacing*2)})
+			.text(function(d) {return formatDate(d)})
+			.attr("text-anchor", "start") 
+			// .style("font-weight", "lighter")
+			.style("fill", "black")
+			// .style("stroke", "white") 
+			// .style("stroke-width", "0.5px")		
+			.style("z-index", 1000)				
 
 	 	
 	  	// var sz = (fullHeight - fullMargin.bottom - fullMargin.top)/years.length;
 
-	  	//width calc
+	  	//width calc height calc
 	  	//console.log(d3.max(data, function(d) { return +d["y-key"]})*sz + fullMargin.left + fullMargin.right)
           console.log("height", d3.max(data, function(d) { return +d["y-key"]})*sz + fullMargin.top + fullMargin.bottom)
 
@@ -300,7 +355,7 @@ function loadTimeline(choices, filter) {
 		 .html(function(d) {
 		  // console.log(d);
 		   return "<div>" +
-		           "<span style='color:black; font-weight:600'>" + formatDate(d.start) + "</span>" +
+		           "<span style='color:black; font-weight:600'>" + formatDate(d.yr_cleaned) + "</span>" +
 		           // "<span style='color:black; font-weight:600'>" + d.yr_cleaned + "</span>"  +
 		           "<span style='color:" + color(d.era) + "; font-weight:600'> (" + d.era + ")</span><br/>" +
 		           "<span style='color:black'>" + d.event_cleaned + "</span><br/><br/>" +
@@ -316,14 +371,14 @@ function loadTimeline(choices, filter) {
 		 .attr("class", "rect-event")
 		 .attr("x", function(d, i) {
 				if (+d["sub-key"] <= 0) {	 			
-                    return x(Math.floor((+d["sub-key"]))) + sz/(spacing*2);
+                    return x(Math.floor((+d["sub-key"]))) + sz/(spacing*4);
 		 		}
 		 		else {
-		 			return x(Math.floor((+d["sub-key"]))) - sz/(spacing*2);
+		 			return x(Math.floor((+d["sub-key"]))) - sz/(spacing*4);
 		 		}
 		 })
 		 .attr("y", function(d, i) {
-		 		return y(d["start"])
+		 		return y(d["yr_cleaned"])
 		 })
 		 .attr("width", sz/spacing)
 		 .attr("height", sz/spacing)
@@ -348,16 +403,16 @@ function loadTimeline(choices, filter) {
          .style("position", "absolute")
 		 .style("left", function(d, i) {
 				if (+d["sub-key"] <= 0) {	
-                    let w = x(Math.floor((+d["sub-key"]))) + sz/(spacing*2) + fullMargin.left; 			
+                    let w = x(Math.floor((+d["sub-key"]))) + sz/(spacing*4) + fullMargin.left; 			
                     return w + 'px';
 		 		}
 		 		else {
-                    let w = x(Math.floor((+d["sub-key"]))) - sz/(spacing*2) + fullMargin.left;
+                    let w = x(Math.floor((+d["sub-key"]))) - sz/(spacing*4) + fullMargin.left;
 		 			return w + 'px';
 		 		}
 		 })
 		 .style("top", function(d, i) {
-                let h = y(d["start"]);
+                let h = y(d["yr_cleaned"]);
 		 		return h + 'px'
 		 })
 		 .style("width", sz/spacing)
@@ -371,50 +426,6 @@ function loadTimeline(choices, filter) {
             return "<img src='" + d["img-path"] + "' alt='Image " + d["sub-key"] + d["y-key"] + "' width='" + sz/spacing + "' height='" + sz/spacing + "'>";
 
           });         
-    // Create patterns dynamically based on unique imgPath values
-    var patterns = svg.selectAll(".pattern")
-    .data(data, function(d) { return "/img/allergies.png"; })
-    .enter().append("pattern")
-    .attr("id", function(d) { return "pattern-" + d["img-path"]; })
-    .attr("class", "pattern")
-    .attr("width", "100%")
-    .attr("height", "100%");
-
-    // Append images to patterns
-    patterns.append("image")
-    .attr("x", 0)
-    .attr("y", 0)
-    .attr("width", sz / spacing)
-    .attr("height", sz / spacing)
-    .attr("xlink:href", function(d) { return d['img-path']; });        
-
-		svg.append("line")
-			.attr("id", "vline")
-		 	.attr("x1", x(0) + sz/(spacing*2))
-		 	.attr("x2", x(0) + sz/(spacing*2))  
-		 	.attr("y1", 0)
-		 	.attr("y2", fullHeight - fullMargin.top - fullMargin.bottom) 
-		 	.attr("stroke", "black")
-		 	.attr("stroke-width", 1)	
-		 	.attr("tranform", "translate(" + sz + "," + sz + ")")
-
-		svg.append("line")
-		 	.attr("x1", x(0) - 30 + sz/(spacing*2))
-		 	.attr("x2", x(0) + 30 + sz/(spacing*2))  
-		 	.attr("y1", 0 - 5)
-		 	.attr("y2", 0 - 5) 
-		 	.attr("stroke", "black")
-		 	.attr("stroke-width", 1)	
-		 	// .attr("tranform", "translate(" + sz/2 + "," + -sz + ")")		 	
-
-
-		svg.append("line")
-		 	.attr("x1", x(0) - 30 + sz/(spacing*2))
-		 	.attr("x2", x(0) + 30 + sz/(spacing*2))  
-		 	.attr("y1", fullHeight - fullMargin.top - fullMargin.bottom + 5)
-		 	.attr("y2", fullHeight - fullMargin.top - fullMargin.bottom + 5)
-		 	.attr("stroke", "black")
-		 	.attr("stroke-width", 1)	
 		 	// .attr("tranform", "translate(" + sz/2 + "," + -sz + ")")		 	
 
 
@@ -446,64 +457,64 @@ loadTimeline(filter_list, "era_cleaned");
 
 var checkBox = d3.selectAll(".filter-check")
 
-checkBox.on("change", function () {
-	d3.selectAll('.movie-rect').remove();
+// checkBox.on("change", function () {
+// 	d3.selectAll('.movie-rect').remove();
 
-	var choices = []
-	var checkboxes = document.querySelectorAll('input[type=checkbox]')
-	var unchecked = []
+// 	var choices = []
+// 	var checkboxes = document.querySelectorAll('input[type=checkbox]')
+// 	var unchecked = []
 
-	for (var i = 0; i < checkboxes.length; i++) {
-		if (checkboxes[i]["checked"]) {
-			choices.push((checkboxes[i].value))
-		}
-	    else {
-	    	unchecked.push((checkboxes[i].value))
-	    }
-	}
+// 	for (var i = 0; i < checkboxes.length; i++) {
+// 		if (checkboxes[i]["checked"]) {
+// 			choices.push((checkboxes[i].value))
+// 		}
+// 	    else {
+// 	    	unchecked.push((checkboxes[i].value))
+// 	    }
+// 	}
 
-	for (var i = 0; i <unchecked.length; i ++) {
-		d3.select("#" + unchecked[i].split(' ').join('_') + "sabers").selectAll(".bar").style("box-shadow", "none").style("border-right-color", "#d6d6d6")
-	}
+// 	for (var i = 0; i <unchecked.length; i ++) {
+// 		d3.select("#" + unchecked[i].split(' ').join('_') + "sabers").selectAll(".bar").style("box-shadow", "none").style("border-right-color", "#d6d6d6")
+// 	}
 
-	for (var i = 0; i <choices.length; i ++) {
-		d3.select("#" + choices[i].split(' ').join('_') + "sabers").selectAll(".bar")
-			.style("box-shadow", function(d) {
-				return "0.4em 0 0.6em 0.1em " + media_color(d);
-			})
-			.style("border-right-color", function(d) {
-				return media_color(d);
-			})
-	}
+// 	for (var i = 0; i <choices.length; i ++) {
+// 		d3.select("#" + choices[i].split(' ').join('_') + "sabers").selectAll(".bar")
+// 			.style("box-shadow", function(d) {
+// 				return "0.4em 0 0.6em 0.1em " + media_color(d);
+// 			})
+// 			.style("border-right-color", function(d) {
+// 				return media_color(d);
+// 			})
+// 	}
 
-	var form = document.getElementById("color_filter")
-	var form_val;
-	for(var i=0; i<form.length; i++) {		
-		if (form[i].checked) {
-			form_val = form[i].id;
-		}		
-	}	
-	loadTimeline(choices, form_val);	 
-})
+// 	var form = document.getElementById("color_filter")
+// 	var form_val;
+// 	for(var i=0; i<form.length; i++) {		
+// 		if (form[i].checked) {
+// 			form_val = form[i].id;
+// 		}		
+// 	}	
+// 	loadTimeline(choices, form_val);	 
+// })
 
-var radioButtons = d3.selectAll(".filter-radio")
+// var radioButtons = d3.selectAll(".filter-radio")
 
-radioButtons.on("change", function() {	
-	d3.selectAll('.movie-rect').remove();
+// radioButtons.on("change", function() {	
+// 	d3.selectAll('.movie-rect').remove();
 
-	var choices = []
-	var checkboxes = document.querySelectorAll('input[type=checkbox]:checked')
-	for (var i = 0; i < checkboxes.length; i++) {
-	    choices.push(checkboxes[i].value)
-	}
+// 	var choices = []
+// 	var checkboxes = document.querySelectorAll('input[type=checkbox]:checked')
+// 	for (var i = 0; i < checkboxes.length; i++) {
+// 	    choices.push(checkboxes[i].value)
+// 	}
 
-	var form = document.getElementById("color_filter")
-	var form_val;
-	for(var i=0; i<form.length; i++) {		
-		if (form[i].checked) {
-			form_val = form[i].id;
-		}
-	}	
+// 	var form = document.getElementById("color_filter")
+// 	var form_val;
+// 	for(var i=0; i<form.length; i++) {		
+// 		if (form[i].checked) {
+// 			form_val = form[i].id;
+// 		}
+// 	}	
 
-	loadTimeline(choices, form_val);
-})
+// 	loadTimeline(choices, form_val);
+// })
