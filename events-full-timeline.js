f73458abfdd420f0f0547f93af0f90f192ce8f48
Anjali <ashrivastava@berkeley.edu>
2023-12-25 15:08:12 -0800

    labels

--- events-full-timeline.js
+++ events-full-timeline.js
@@ -102,6 +102,13 @@ function loadTimeline(choices, filter) {
 
 		data = data.sort(function(a,b) {return a.yr_cleaned - b.yr_cleaned});
 		var years = d3.set(data.map(function(d) {return d.yr_cleaned})).values();
+		// years.push(2024)
+		years.sort(function(a, b) {
+			return b - a;
+		});			
+		console.log(years)
+		//append 2024 to years
+		// years.push(2024)
 		// var sz = (fullHeight - fullMargin.bottom - fullMargin.top)/years.length;        
         var sz = (fullWidth - fullMargin.left - fullMargin.right)/years.length;
         console.log(sz)
@@ -135,11 +142,9 @@ function loadTimeline(choices, filter) {
 		    data = csv; // so that no boxes checked shows all data
 		}	
 
-		var years = d3.set(data.map(function(d) {return d.yr_cleaned})).values();
+		// var years = d3.set(data.map(function(d) {return d.yr_cleaned})).values();
 
-		years.sort(function(a, b) {
-			return b - a;
-		});		
+	
 
 		var y = d3.scaleBand()
 			.domain(years)
@@ -370,11 +375,15 @@ function loadTimeline(choices, filter) {
 		 .append("rect")
 		 .attr("class", "rect-event")
 		 .attr("x", function(d, i) {
+				let w = sz/(spacing*4);
+				if (+d["anno_flag"] == 1) {
+					w -= sz/2;
+				}
 				if (+d["sub-key"] <= 0) {	 			
-                    return x(Math.floor((+d["sub-key"]))) + sz/(spacing*4);
+                    return x(Math.floor((+d["sub-key"]))) + w;
 		 		}
 		 		else {
-		 			return x(Math.floor((+d["sub-key"]))) - sz/(spacing*4);
+		 			return x(Math.floor((+d["sub-key"]))) - w;
 		 		}
 		 })
 		 .attr("y", function(d, i) {
@@ -418,16 +427,53 @@ function loadTimeline(choices, filter) {
 		 .style("width", sz/spacing)
 		 .style("height", sz/spacing)
 		 .attr("z-index", 20)
-        //  .style("background-image", "url(/img/allergies.png)")         
-		 // .attr("height", y.bandwidth()*.8)     
-
 
          containers.html(function(d) {
             return "<img src='" + d["img-path"] + "' alt='Image " + d["sub-key"] + d["y-key"] + "' width='" + sz/spacing + "' height='" + sz/spacing + "'>";
-
           });         
-		 	// .attr("tranform", "translate(" + sz/2 + "," + -sz + ")")		 	
+		
+		svg.append("line")
+		  .attr("id", "vline-dotted")
+		   .attr("x1", x(0))
+		   .attr("x2", x(0))  
+		   .attr("y1", y(2023))
+		   .attr("y2", y(2021) + sz/2) 
+		   .attr("stroke", "black")
+		   .attr("stroke-width", 1)	
+		   .style("stroke-dasharray", ("1, 3"))
+		//    .attr("tranform", "translate(" + sz + "," + sz + ")")
 
+		var circleData = [{"year": 2021, "y_adjust": sz/2, "color": "#4C4082"}, {"year": 2022, "y_adjust": sz/2, "color": "#800035"}, {"year": 2023, "y_adjust": 0, "color": "#800035"}]
+		svg.selectAll("mycircle")
+			 .data(circleData)
+			 .enter()
+			 .append("circle")
+			   .attr("cx", function(d) { return x(0); })
+			   .attr("cy", function(d) { return y(d.year) + d.y_adjust; })
+			   .attr("r", "6")
+			   .style("fill", function(d) { return d.color; })		
+			//    .style("opacity", 0.75)	
+
+		svg.append("text")
+			.text("Joined Adobe")
+			.attr("class", "svg-label")
+			.attr("x", x(0) - 10)
+			.attr("y", y(2022) + sz/2 + 2)
+			.attr("text-anchor", "end")
+			.attr("alignment-baseline", "middle")
+			.style("font-size", "15px")
+			.style("font-family", "Lato")
+
+		svg.append("text")
+			.text("Joined ThermoFisher Scientific")
+			.attr("class", "svg-label")
+			.attr("x", x(0) - 10)
+			.attr("y", y(2021) + sz/2 + 2)
+			.attr("text-anchor", "end")
+			.attr("alignment-baseline", "middle")
+			.style("font-size", "15px")
+			.style("font-family", "Lato")			
+			// .style("font-weight", 499)
 
 		svg.append("text")
 			.text("PROFESSIONAL")
